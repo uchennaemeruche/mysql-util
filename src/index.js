fdabb4f6731a5db2957794630138f5c83c90fd51
Uchenna Emeruche <emerucheuchenna@gmail.com>
2021-01-06 15:11:48 +0100

    package rewritten in es6 and new methods exposed

--- src/index.js
+++ src/index.js
@@ -1,149 +1,257 @@
-
-// const {queryDb, setConnection} = require('./models/query.model');
-
 const mysql = require("mysql");
 
-let connection;
+let connectionObj = {
+  connection: null,
+  conn: async function (con) {
+    this.connection = con;
+    return this.conneciton;
+  },
+  async getConn() {
+    return await this.connection;
+  },
+};
 
-async function setConnection({host, user, password, database, connectionLimit}){
-    connection  = await mysql.createPool({
-        host: host,
-        user: user,
-        password: password,
-        database: database,
-        connectionLimit: connectionLimit
-    });
-}
+exports.setConnection = async ({
+  host,
+  user,
+  password,
+  database,
+  connectionLimit,
+}) => {
+  let conncectionString = await mysql.createPool({
+    host,
+    user,
+    password,
+    database,
+    connectionLimit,
+  });
+  await connectionObj.conn(conncectionString);
+};
 
-function checkParamType(data){
-    if(typeof data == 'string') return `'${data}'`;
-    else return `${data}`;
-}
+let _required = (val) => {
+  throw new Error(`${val} is required`);
+};
 
-function matchQueryFilters(query, params){
+let checkParamType = (data) => {
+  if (typeof data == "string") return `'${data}'`;
+  else return `${data}`;
+};
+
+let matchQueryFilters = (query, params) => {
+  let counter = 0;
+  params[0].unshift("");
+  for (let param in params) {
+    query +=
+      `${params[param][1]} ${params[param][2]} ` +
+      checkParamType(params[param][3]);
+    counter++;
+    if (counter < params.length) {
+      query += ` ${params[counter][0]} `;
+    }
+  }
+  return query;
+};
+
+let checkOptionsType = (options, op) => {
+  let query;
+  if (options.length == 1 && typeof options[0] != "string") {
+    let values = options[0];
+    values.queryKeyword = op;
+    query = constructQuery(values);
+  } else {
+    if (op == "select")
+      query = constructQuery({
+        queryKeyword: op,
+        tableName: options[0],
+        fields: options[1],
+        params: options[2],
+      });
+    else if (op == "insert")
+      query = constructQuery({
+        queryKeyword: op,
+        tableName: options[0],
+        data: options[1],
+      });
+    else if (op == "update")
+      query = constructQuery({
+        queryKeyword: op,
+        data: options[1],
+        tableName: options[0],
+        params: options[2],
+      });
+    else if (op == "delete")
+      query = constructQuery({
+        queryKeyword: op,
+        tableName: options[0],
+        params: options[1],
+      });
+  }
+  return query;
+};
+
+module.exports.select = (...options) => {
+  let query = checkOptionsType(options, "select");
+  console.log("QUery:", query);
+  return runQuery(query);
+};
+
+module.exports.insert = (...options) => {
+  let query = checkOptionsType(options, "insert");
+  return runQuery(query);
+};
+
+module.exports.update = (...options) => {
+  let query = checkOptionsType(options, "update");
+  return runQuery(query);
+};
+
+module.exports.delete = (...options) => {
+  let query = checkOptionsType(options, "delete");
+  return runQuery(query);
+};
+
+let constructQuery = ({
+  queryKeyword = _required("queryType"),
+  fields = null,
+  tableName = _required("tableName"),
+  data,
+  params = null,
+}) => {
+  const keyWords = {
+    SELECT: "select",
+    UPDATE: "update",
+    DELETE: "delete",
+    INSERT: "insert",
+  };
+
+  let query = ``;
+
+  if (queryKeyword == keyWords.SELECT) {
+    if (fields == null || fields == "*") query = `SELECT *`;
+    else {
+      query = `SELECT `;
+      let fieldsLength = fields.length;
+      let counter = 0;
+      fields.forEach((field) => {
+        query += `${field}`;
+        counter++;
+        if (counter < fieldsLength) {
+          query += `, `;
+        }
+      });
+    }
+
+    if (params == null) query += ` FROM ${tableName}`;
+    else {
+      query += ` FROM ${tableName} WHERE `;
+      query = matchQueryFilters(query, params);
+
+      //   query = `SELECT * FROM ${tableName} WHERE `;
+      //   query = matchQueryFilters(query, params);
+    }
+  } else if (queryKeyword == keyWords.UPDATE) {
+    query = `UPDATE ${tableName} SET `;
+    let fieldLength = Object.entries(data).length;
     let counter = 0;
-    params.where[0].unshift("");
-    for(let param in params.where){
-        query +=  `${params.where[param][1]} ${ params.where[param][2]} ` + checkParamType(params.where[param][3]);
-        counter ++;
-        if(counter < params.where.length){
-            query += ` ${params.where[counter][0]} `;
-        }  
+    for (let field in data) {
+      query += `${field} = ` + checkParamType(data[field]);
+      counter++;
+      if (counter < fieldLength) query += `, `;
     }
-    return query;
-}
+    counter = 0;
+    query += ` WHERE `;
 
+    query = matchQueryFilters(query, params);
+  } else if (queryKeyword == keyWords.INSERT) {
+    let fieldLength = Object.entries(data).length;
+    let counter = 0;
 
-function constructQuery(queryKeyword, tableName, requestBody, params = null){
-    const keyWords = {
-        SELECT: 'select',
-        UPDATE: 'update',
-        DELETE: 'delete',
-        INSERT: 'insert'
-    };
-
-    let query = ``;
-
-    if (queryKeyword == keyWords.SELECT){
-        if(params == null)
-            query = `SELECT * FROM ${tableName}`;
-        else{
-            query = `SELECT * FROM ${tableName} WHERE `;
-            query = matchQueryFilters(query, params);
-        }
-    }else if(queryKeyword == keyWords.UPDATE){
-        query = `UPDATE ${tableName} SET `;
-        let fieldLength = Object.entries(requestBody).length;
-        let counter = 0;
-        for(let field in requestBody){
-            query += (`${field} = ` + checkParamType(requestBody[field]));
-            counter ++;
-            if(counter < fieldLength) 
-                query += `, `;
-        }
+    query = `INSERT INTO ${tableName} (`;
+    for (let field in data) {
+      query += `${field}`;
+      counter++;
+      if (counter < fieldLength) query += `, `;
+      else {
+        query += `) VALUES(`;
         counter = 0;
-        query += ` WHERE `;
-
-        query = matchQueryFilters(query, params);
-
-    }else if(queryKeyword == keyWords.INSERT){
-        let fieldLength = Object.entries(requestBody).length;
-        let counter = 0;
-
-        query = `INSERT INTO ${tableName} (`;
-        for(let field in requestBody){
-            query += (`${field}`)
-            counter ++;
-            if(counter < fieldLength) 
-                query += `, `;
-            else{
-                query += `) VALUES(`;
-                counter = 0;
-                for(let value in requestBody){
-                    query +=    checkParamType(requestBody[value]);
-                    counter ++;
-                    if(counter < fieldLength){
-                        query += `, `;
-                    }else{
-                        query += `)`;
-                    }
-                }
-            }
-               
+        for (let value in data) {
+          query += checkParamType(data[value]);
+          counter++;
+          if (counter < fieldLength) {
+            query += `, `;
+          } else {
+            query += `)`;
+          }
         }
-    }else if(queryKeyword == keyWords.DELETE){
-        if(params == null)
-            query = `DELETE FROM ${tableName}`;
-        else{
-            query = `DELETE FROM ${tableName} WHERE `;
+      }
+    }
+  } else if (queryKeyword == keyWords.DELETE) {
+    if (params == null) query = `DELETE FROM ${tableName}`;
+    else {
+      query = `DELETE FROM ${tableName} WHERE `;
 
-            query = matchQueryFilters(query, params)
+      query = matchQueryFilters(query, params);
+    }
+  }
+  return query;
+};
+
+let runQuery = (query) => {
+  return new Promise((resolve) => {
+    try {
+      queryDb(query, (err, result) => {
+        if (err) {
+          resolve(err);
+        } else {
+          resolve(result);
         }
+      });
+    } catch (err) {
+      resolve(err.message);
     }
-    return query;
-}   
+  });
+};
 
-let runQuery = function(queryType, tableName, requestBody, params) {
-
-    let query = constructQuery(queryType, tableName, requestBody, params);
-    return new Promise((resolve) => {
-    try{
-        queryDb(query, (err, result) =>{
-            if(err){
-                resolve(err);
-            }
-            else{
-                resolve(result);
-            }
-        });
-    }catch(err){
-        resolve(err.message);
+module.exports.query = (queryType, tableName, fields, data, params) => {
+  let query = constructQuery({
+    queryKeyword: queryType,
+    fields,
+    tableName,
+    data,
+    params,
+  });
+  return new Promise((resolve) => {
+    try {
+      queryDb(query, (err, result) => {
+        if (err) {
+          resolve(err);
+        } else {
+          resolve(result);
+        }
+      });
+    } catch (err) {
+      resolve(err.message);
     }
+  });
+};
+
+let queryDb = (query, result) => {
+  let getConnectionFromPool = async function () {
+    await connectionObj.getConn();
+    connectionObj.connection.query(query, (err, res) => {
+      if (err) {
+        if (err.code == "ECONNREFUSED") {
+          getConnectionFromPool();
+          return;
+        }
+        result(err, null);
+        return;
+      }
+      if (res) {
+        result(null, res);
+        return;
+      }
     });
-}
-
-
-function queryDb(query, result){
-    let getConnectionFromPool = function(){
-        connection.query(query, (err, res) =>{
-            if(err){
-                if(err.code == "ECONNREFUSED"){
-                    getConnectionFromPool();
-                    return;
-                }
-                // console.log("An Error occured")
-                // console.log(err);
-                result(err, null);
-                return;
-            }
-            if(res){
-                result(null, res);
-                return;
-            }
-        });
-    }
-    getConnectionFromPool();
-}
-
-exports.setConnection = setConnection;
-exports.runQuery = runQuery;
+  };
+  getConnectionFromPool();
+};
--- test/index.js
+++ test/index.js
@@ -1,84 +1,88 @@
 const dotenv = require("dotenv");
 dotenv.config();
-const faker = require('faker');
-const mysqlUtil = require('../src/index');
-
-
-
-var params = {
-  where:[
-      ['age','!=', 13],
-      ['AND', 'gender', 'like', 'male'],
-      ['OR', 'id', '=', 1]
-  ]
-};
-var deleteParams = {
-  where:[
-   ['age','=',23],
-   ['OR', 'name', 'like', '%Field Name%']
-  ]
-};
-const updateParams = {
-  where:[
-    ['id','=', '22']
-  ]
-}
+const faker = require("faker");
+const mysqlUtil = require("../src/index");
 
 const updateBody = {
-  "gender": "male"
+  gender: "male",
 };
 
 const reqBody = {
-  "name": faker.name.firstName() + " " + faker.name.lastName(),
-  "age": Number((faker.random.number()).toString().slice(0,2)),
-  "gender": faker.name.gender() == 'Man' ? 'Male' : 'Female'
+  name: faker.name.firstName() + " " + faker.name.lastName(),
+  age: Number(faker.random.number().toString().slice(0, 2)),
+  gender: faker.name.gender() == "Man" ? "Male" : "Female",
 };
 
 const testQueries = [
   {
-    input:["insert", "users", reqBody],
-    description: "should insert a new user and return result"
+    input: ["insert", "users", "", reqBody, ""],
+    description: "should insert a new user and return result",
+  },
+
+  {
+    description: "Should return all columns and rows in the user table",
+    input: ["select", "users"],
   },
   {
-    input:["select", "users", "null", params],
-    description: "should return a specific user"
+    description:
+      "Should return the name and age of a specific user whose ID =1",
+    input: ["select", "users", ["name", "age"], "", [["id", "=", 1]]],
   },
   {
-    input:["select", "users"],
-    description: "should return all users"
+    description: "Should return all male users above 40 years",
+    input: [
+      "select",
+      "users",
+      "*",
+      "",
+      [
+        ["gender", "like", "male"],
+        ["AND", "age", ">", 40],
+      ],
+    ],
   },
-  { 
-    input:["delete", "users", null, deleteParams],
-    description: "should delete all users with age 40 and above"
+
+  {
+    input: ["delete", "users", "_", "", [["id", "=", "50"]]],
+    description: "should delete all users whose age is 50",
   },
   {
-    input:["update", "users", updateBody, updateParams],
-    description: "Update user with id:22"
-  }
+    input: ["update", "users", "", updateBody, [["id", "=", "51"]]],
+    description: "should update gender of user with id:52",
+  },
 ];
 
-beforeEach(function(){
+beforeEach(function () {
   mysqlUtil.setConnection({
+    // eslint-disable-next-line no-undef
     host: process.env.DB_HOST,
+    // eslint-disable-next-line no-undef
     user: process.env.DB_USER,
+    // eslint-disable-next-line no-undef
     password: process.env.DB_PASSWORD,
+    // eslint-disable-next-line no-undef
     database: process.env.DB_NAME,
-    connectionLimit: 25
+    connectionLimit: 25,
   });
 });
-describe("#test-queries()", () =>{
-  testQueries.forEach((test) =>{
-    it(test.description, () =>{
-      var [queryType, tableName, requestBody, params] = test.input;
-      mysqlUtil.runQuery(queryType, tableName, requestBody, params).then(data =>{
-        console.log(data);
-        return data;
-      }).catch(err =>{
-        console.log(err);
-        return err;
-      });
+
+describe("#test-queries()", () => {
+  testQueries.forEach((test) => {
+    it(test.description, () => {
+      // var [tableName, fields, params] = test.input;
+      var [queryType, tableName, fields, data, params] = test.input;
+
+      mysqlUtil
+        .query(queryType, tableName, fields, data, params)
+        // .select(tableName, fields, params)
+        .then((res) => {
+          console.log("Data: ", res);
+          return res;
+        })
+        .catch((err) => {
+          console.log("Error: ", err);
+          return err;
+        });
     });
   });
 });
-
-
